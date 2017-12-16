2e3c8e2b4efd5bc1ceb8eb8ab8cf4ad10590c003
Svetlana Subbotina <subbotina@phystech.edu>
2017-12-16 18:49:00 +0300

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/Room.java
+++ src/main/java/ru/sbt/mipt/oop/Room.java
@@ -2,7 +2,7 @@ package ru.sbt.mipt.oop;
 
 import java.util.Collection;
 
-public class Room {
+public class Room implements Actionable {
     private Collection<Light> lights;
     private Collection<Door> doors;
     private String name;
@@ -23,5 +23,16 @@ public class Room {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public void executeAction(Action action) {
+        for (Light light : lights) {
+            light.executeAction(action);
+        }
+        for (Door door : doors) {
+            door.executeAction(action);
+        }
+        action.execute(this);
     }
 }
